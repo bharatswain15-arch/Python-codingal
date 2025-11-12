9a951be364a7d84d1d0fde450a9f3a5f4f0261cc
bharatswain15-arch <bharatswain15@gmail.com>
2025-11-12 18:22:02 +0000

    Add files via upload

---
+++ activity_1.py
@@ -0,0 +1,11 @@
+tree1 = 98
+tree2 = 94
+tree3 = 90
+tree4 = 86
+tree5 = 82
+
+sum = tree1 + tree2 + tree3 + tree4 + tree5
+print("sum of all the 5 trees is ",sum)
+
+average = sum / 5
+print("The average of all the trees is ",average)
