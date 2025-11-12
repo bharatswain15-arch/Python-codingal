173726daf860e00cd35e722f3ad2ea3c12dd475b
bharatswain15-arch <bharatswain15@gmail.com>
2025-11-12 18:36:51 +0000

    Add files via upload

---
+++ activity_3.py
@@ -0,0 +1,7 @@
+math = int(input("math: "))
+english = int(input("english: "))
+science = int(input("science: "))
+
+sum = math + english + science
+percentage = (sum / 300) * 100
+print(percentage)
