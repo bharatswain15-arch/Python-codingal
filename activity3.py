17d6ad192c88503804b3658ae5ed478d29cdc1e1
bharatswain15-arch <bharatswain15@gmail.com>
2025-11-19 18:32:19 +0000

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,4 +1,8 @@
-text = input("enter a string")
-print(text)
-text = text[::-1]
-print(text)
+i = int(input("enter a number: "))
+if i < 15:
+    print("i < 15")
+    print("i am in if block")
+else :
+    print("i > 15")
+    print("i am in an else block")
+print("i'm not in if and not in else Block")
