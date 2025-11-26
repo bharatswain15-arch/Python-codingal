b34c8084df66c06bcca6da85bfbe87b53193ff83
bharatswain15-arch <bharatswain15@gmail.com>
2025-11-26 18:35:51 +0000

    Add files via upload

--- activity3.py
+++ activity3.py
@@ -1,8 +1,15 @@
-i = int(input("enter a number: "))
-if i < 15:
-    print("i < 15")
-    print("i am in if block")
-else :
-    print("i > 15")
-    print("i am in an else block")
-print("i'm not in if and not in else Block")
+height = float(input("Enter your height in cm: "))
+weight = float(input("Enter your height in kg: "))
+BMI = weight / (height / 100) **2
+print("your BMI is  ",BMI)
+
+if BMI <= 18.4:
+    print("You are underweight")
+elif BMI <= 24.9:
+    print("You are healthy")
+elif BMI <= 29.9:
+    print("you are overweight")
+elif BMI <= 34.9:
+    print("You are severely overweight")
+else:
+    print("You are unhealthy")
